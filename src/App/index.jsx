9f2970413300a4db74741f9d4329bd2faa9be635
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-14 16:43:40 +0100

    Fixed lint warnings

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,17 +2,12 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-function App() {
-
-
-
+export default function App() {
   return (
     <>
       <Header />
       <Main />
       <Footer />
     </>
-  )
+  );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -20,5 +20,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </ThemeProvider>
       </I18nextProvider>
     </ParallaxProvider>
-  </StrictMode>
+  </StrictMode>,
 );
