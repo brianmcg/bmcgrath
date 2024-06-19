f2ea22d913b00e093476d53fcecd104866ab2752
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-20 00:33:24 +0100

    Updated url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,4 @@
-export const SEND_MAIL_URL = 'https://bmcgrath.centralindia.cloudapp.azure.com/emails/send';
+export const SEND_MAIL_URL = 'https://bmcgrath.centralindia.cloudapp.azure.com/api/emails/send';
 
 export const RUGBY_RANKINGS_URL = 'http://www.bmcgrath.net/rugby-rankings/';
 
